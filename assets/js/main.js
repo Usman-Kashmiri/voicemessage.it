5316b5b2e39235e55bff153059227f776011267f
Usman-Kashmiri <usmankashmiri378@gmail.com>
2022-06-15 18:58:53 +0500

    Added thanks modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,8 +17,6 @@ var audio_duration = Math.floor(audio.duration);
 var audio_time = Math.floor(audio.currentTime);
 
 function update_time(current_time, audio_duration){
-    // $("#currentAudioTime").html(format_time(current_time));
-    // $("#totalAudioDuration").html(format_time(audio_duration));
     $('#duration').text('0:0'+ current_time + ' / 0:0' + audio_duration)
 }
 
@@ -31,11 +29,7 @@ audio.ontimeupdate = function() {
 //     return !audio.paused;
 //   };
 
-$('.skip-btn').click(function() {
-    alert(audio.currentTime="seconds")
-})
-
-$('.play-pause-btn').click(function() {
+$('.pause-play-btn').click(function() {
     if(audio.paused){
         audio.play();
         $('#pause-play-icon').addClass('fa-pause').removeClass('fa-play');
@@ -49,8 +43,11 @@ $('.play-pause-btn').click(function() {
     // }, 10000)
 })
 const recordButton = document.getElementById("record-btn");
-// set preview
-const preview = document.getElementById("audio-playback");
+
+const recording = document.getElementById("audio-playback");
+
+$('#recording-duration-and-visualizier').hide();
+$('.save-or-delete').hide();
 
 $('.record-btn').click(function() {
     recordButton.disabled = true
@@ -68,10 +65,10 @@ $('.record-btn').click(function() {
         audio_stream = stream;
         recorder = new MediaRecorder(stream);
 
-        // when there is data, compile into object for preview src
+        // when there is data, compile into object for recording src
         recorder.ondataavailable = function (e) {
             const url = URL.createObjectURL(e.data);
-            preview.src = url;
+            recording.src = url;
         };
         recorder.start();
 
@@ -80,9 +77,43 @@ $('.record-btn').click(function() {
             stopRecording();
             $('.progress-circle').hide()
             $('.recording-duration').hide()
+            $('.recording-modal-btns').html(
+                `<button id="play-pause-btn" class="record-btn d-flex flex-column justify-content-center align-items-center">
+                    <span class="d-flex justify-content-center align-items-center">
+                        <i id="play-icon" class="fa fa-play"></i>
+                    </span>
+                </button>`
+            )
+            var recording = $('#audio-playback')[0];
+
+            var recording_duration = Math.floor(recording.duration);
+            var recording_time = Math.floor(recording.currentTime);
+
+            function update_time(current_time, recording_duration){
+                $('#recording-duration').text('0:0'+ current_time + ' / 0:09')
+            }
+
+            recording.ontimeupdate = function() {
+                update_time(Math.floor(this.currentTime), Math.floor(this.duration))
+            }
+            $('#play-pause-btn').click(function() {
+                if(recording.paused){
+                    recording.play();
+                    $('#play-icon').addClass('fa-pause').removeClass('fa-play');
+                  } else {
+                    recording.pause();
+                    $('#play-icon').addClass('fa-play').removeClass('fa-pause');
+                  }
+                  $('#recording-duration-and-visualizier').show();
+            })
+            $('.save-or-delete').show();
         }, 10000);
     });
 })
+
+$('#save-btn').click(function() { 
+    $('#recordingModal').stopPropagation();
+ })
 
 function stopRecording() {
     recorder.stop();
